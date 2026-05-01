070bb57e274377ec190e8b4120b3c44648edd65e
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-05-01 18:51:28 +0400

    ok

--- app/routes.ts
+++ app/routes.ts
@@ -19,5 +19,24 @@ export default [
     path: "/api/wishlist",
     file: "routes/api.wishlist.ts",
   },
-
+{
+  path: "/api/webhooks/app/uninstalled",
+  file: "routes/api/webhooks/app/uninstalled.tsx",
+},
+{
+  path: "/api/webhooks/app/scopes_update",
+  file: "routes/api/webhooks/app/scopes_update.tsx",
+},
+{
+  path: "/api/webhooks/customers/data_request",
+  file: "routes/api/webhooks/customers/data_request.tsx",
+},
+{
+  path: "/api/webhooks/customers/redact",
+  file: "routes/api/webhooks/customers/redact.tsx",
+},
+{
+  path: "/api/webhooks/shop/redact",
+  file: "routes/api/webhooks/shop/redact.tsx",
+},
 ];
