e44e77099d60a6436dd2821b120f5827cf63f4fb
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-05-01 12:28:59 +0400

    ok

--- app/routes/webhooks.compliance.tsx
+++ app/routes/webhooks.compliance.tsx
@@ -1,8 +1,19 @@
 import type { ActionFunctionArgs } from "react-router";
+import crypto from "crypto";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const body = await request.text();
-  console.log("Compliance webhook:", body);
 
-  return new Response("OK");
+  const hmacHeader = request.headers.get("x-shopify-hmac-sha256");
+
+  const digest = crypto
+    .createHmac("sha256", process.env.SHOPIFY_API_SECRET || "")
+    .update(body, "utf8")
+    .digest("base64");
+
+  if (digest !== hmacHeader) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  return new Response("OK", { status: 200 });
 };
