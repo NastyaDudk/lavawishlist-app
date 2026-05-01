f4254e4567906f682f42315b1c05a6af28b1f768
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-05-01 18:28:57 +0400

    ok

--- app/routes.ts
+++ app/routes.ts
@@ -19,24 +19,5 @@ export default [
     path: "/api/wishlist",
     file: "routes/api.wishlist.ts",
   },
-  {
-  path: "/webhooks/customers/data_request",
-  file: "routes/webhooks.customers.data_request.tsx",
-},
-{
-  path: "/webhooks/customers/redact",
-  file: "routes/webhooks.customers.redact.tsx",
-},
-{
-  path: "/webhooks/shop/redact",
-  file: "routes/webhooks.shop.redact.tsx",
-},
-{
-  path: "/webhooks/app/uninstalled",
-  file: "routes/webhooks.app.uninstalled.tsx",
-  },
-{
-  path: "/webhooks/app/scopes_update",
-  file: "routes/webhooks.app.scopes_update.tsx",
-},
+
 ];
--- app/routes/api.webhooks.app.scopes_update.tsx
+++ app/routes/api.webhooks.app.scopes_update.tsx
@@ -3,5 +3,5 @@ import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   await authenticate.webhook(request);
-  return new Response("OK");
+  return new Response("OK", { status: 200 });
 };
