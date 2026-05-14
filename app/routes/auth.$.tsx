49856a4bd6b48ee901040fae9c2ba32536d9b927
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-05-14 19:14:24 +0400

    fix auth route

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,14 +1,18 @@
+import type { LoaderFunctionArgs }
+from "react-router";
 
-import type { HeadersFunction, LoaderFunctionArgs } from "react-router";
-import { authenticate } from "../shopify.server";
-import { boundary } from "@shopify/shopify-app-react-router/server";
+import { shopify }
+from "../shopify.server";
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+
+  await shopify.login(request);
 
   return null;
 };
 
-export const headers: HeadersFunction = (headersArgs) => {
-  return boundary.headers(headersArgs);
-};
+export default function Auth() {
+  return null;
+}
