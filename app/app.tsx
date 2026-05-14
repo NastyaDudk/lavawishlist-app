49871c27f3a053819e751705e56d967a053e3a2a
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-05-14 19:02:54 +0400

    ok

--- app/app.tsx
+++ app/app.tsx
@@ -1,10 +1,5 @@
 import { Outlet } from "react-router";
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
 
 export default function App() {
-  return (
-    <AppProvider embedded apiKey="test">
-      <Outlet />
-    </AppProvider>
-  );
+  return <Outlet />;
 }
