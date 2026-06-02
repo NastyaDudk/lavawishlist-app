6cde9857213af08e6822b4651e77c08fe2a1bd46
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-02 21:44:56 +0400

    billing redirect

--- app/routes/app.pricing.tsx
+++ app/routes/app.pricing.tsx
@@ -1,9 +1,16 @@
-import { json } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
 
-export const loader = async () => {
-  return json({
-    ok: true,
+export const loader = async ({ request }: any) => {
+  const { billing } =
+    await authenticate.admin(request);
+
+  const response = await billing.request({
+    plan: "pro",
+    isTest: true,
   });
+
+  return redirect(response.confirmationUrl);
 };
 
 export default function Pricing() {
