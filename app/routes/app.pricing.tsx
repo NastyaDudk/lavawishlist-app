ca7ebe582e0055112db76deac4a8cfbc861826cf
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-02 23:57:30 +0400

    ok

--- app/routes/app.pricing.tsx
+++ app/routes/app.pricing.tsx
@@ -2,12 +2,15 @@ import { redirect } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: any) => {
-  const { billing } = await authenticate.admin(request);
+const { billing, session } = await authenticate.admin(request);
+
+console.log("SESSION:", session);
 
   const response = await billing.request({
-    plan: "pro",
-    isTest: true,
-  } as never);
+  plan: "pro",
+  isTest: true,
+  returnUrl: `https://${session.shop}/admin/apps`,
+} as never);
 
   return redirect((response as any).confirmationUrl);
 };
