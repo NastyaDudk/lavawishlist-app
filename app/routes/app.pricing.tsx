0b82f736e327fc23d266e0a121397a1516e1429b
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-02 21:14:48 +0400

    ok

--- app/routes/app.pricing.tsx
+++ app/routes/app.pricing.tsx
@@ -1,15 +1,11 @@
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: any) => {
-  const { billing } = await authenticate.admin(request);
+  const { billing } =
+    await authenticate.admin(request);
 
   return billing.request({
-    plan: {
-      interval: "EVERY_30_DAYS",
-      amount: 9.99,
-      currencyCode: "USD",
-      name: "Pro Plan",
-    } as any,
+    plan: "pro",
     isTest: true,
   });
 };
