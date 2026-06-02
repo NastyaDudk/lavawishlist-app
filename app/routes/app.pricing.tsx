dfc7442bfea3e6d8f9a598bca2b410a909a61c41
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-02 21:05:36 +0400

    ok

--- app/routes/app.pricing.tsx
+++ app/routes/app.pricing.tsx
@@ -1,7 +1,17 @@
-import { redirect } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
 
-export const loader = async () => {
-  return redirect("https://shopify.com");
+export const loader = async ({ request }: any) => {
+  const { billing } = await authenticate.admin(request);
+
+  return billing.request({
+    plan: {
+      interval: "EVERY_30_DAYS",
+      amount: 9.99,
+      currencyCode: "USD",
+      name: "Pro Plan",
+    } as any,
+    isTest: true,
+  });
 };
 
 export default function Pricing() {
