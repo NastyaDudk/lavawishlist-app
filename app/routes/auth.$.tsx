43cc72857aa891a2a0d025dfc7014d8f327174ae
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-09 15:50:24 +0400

    ok

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,5 +1,12 @@
-export const loader = async () => {
-  return new Response("AUTH ROUTE WORKS");
+import type { LoaderFunctionArgs } from "react-router";
+import { shopify } from "../shopify.server";
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const result = await shopify.login(request);
+
+  return Response.json(result);
 };
 
 export default function Auth() {
