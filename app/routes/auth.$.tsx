2f6c81d00383a1b6716316aec13989f5eae73f22
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-09 18:40:52 +0400

    ok

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -4,8 +4,14 @@ import { shopify } from "../shopify.server";
 export const loader = async ({
   request,
 }: LoaderFunctionArgs) => {
+
+  const url = new URL(request.url);
+
   console.log("AUTH URL:");
-  console.log(new URL(request.url).toString());
+  console.log(url.toString());
+
+  console.log("SHOP:");
+  console.log(url.searchParams.get("shop"));
 
   return await shopify.login(request);
 };
