715c38aec9d4a5036639c4b4db6f70dd59815ce5
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-10 12:55:51 +0400

    ok

--- app/routes/app.pricing.tsx
+++ app/routes/app.pricing.tsx
@@ -1,5 +1,19 @@
-export const loader = async () => {
-  console.log("PRICING ROUTE WORKS");
+import { authenticate } from "../shopify.server";
+import type { LoaderFunctionArgs } from "react-router";
+
+export const loader = async (
+  { request }: LoaderFunctionArgs
+) => {
+
+  const auth =
+    await authenticate.admin(request);
+
+  console.log("AUTH OK");
+
+  console.dir(auth.billing, {
+    depth: null,
+  });
+
   return null;
 };
 
