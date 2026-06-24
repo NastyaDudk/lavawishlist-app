52d6d589603cc0661bc2fee9a88f7ac4458409a6
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2026-06-24 17:55:22 +0400

    ok

--- app/routes.ts
+++ app/routes.ts
@@ -16,10 +16,6 @@ export default [
     path: "/app/dashboard",
     file: "routes/app.dashboard.tsx",
   },
-  {
-  path: "/app/activate-pro",
-  file: "routes/app.activate-pro.tsx",
-},
 {
   path: "/app/pricing",
   file: "routes/app.pricing.ts",
